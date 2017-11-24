c5c30111bc6bb53107199c9f29777f8df8fa1aaf
basoimauryne <32432592+basoimauryne@users.noreply.github.com>
2017-11-24 20:48:31 +0300

    friendlychat

---
+++ LostandFound/app/src/main/java/info/lostandfound/firebase/Constants.java
@@ -0,0 +1,13 @@
+package info.lostandfound.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Constants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constants);
+    }
+}
